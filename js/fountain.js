f1ee516aa9834428a0b94492af493e56bd17c273
garrettlew <garrett.r.lew@gmail.com>
2017-04-13 23:26:19 -0700

    Fountain DONE

--- js/fountain.js
+++ js/fountain.js
@@ -1,15 +1,32 @@
 
+	//LOADING IMAGES
+	var invertedFountain = new Image();
+	invertedFountain.src = "images/inverted_fountain2.jpg";
+
+	var uclaBackground = new Image();
+	uclaBackground.src = "images/ucla_background.jpg";
 
 	var floorHeight = 100;
 
+	//image dimensions
+	var ftnWidth = 500;
+	var ftnHeight = 476;
 	var ftnCanvasEdgeHeight = canvas.height - floorHeight;
+	var ftnX = canvas.width-ftnWidth;
+	var ftnY = ftnCanvasEdgeHeight-ftnHeight;
+
+	//fountain hitbox
+	var teleWidth = 320;
+	var teleHeight = 100;
+	var teleX = 650;
+	var teleY = 376;
 
 	var player1 = {
 		x: 50,
 		y: ftnCanvasEdgeHeight - 100,
 		width: 50,
 		height: 50,
-		speed: 3,
+		speed: 5,
 		velocity: 0,
 		velocityX: 0,
 		grounded: false,
@@ -55,16 +72,16 @@
 	var player1EdgeX = player1.x+player1.width, player1EdgeY = player1.y+player1.height;
 
 	function instructions() {
-		ctx.fillStyle = "#000";
+		ctx.fillStyle = "#fff";
 		ctx.font="20px Arial";
 		ctx.beginPath();
-		ctx.fillText("WASD or Arrow keys to move. Space to restart. P to pause",canvas.width/4,canvas.height/4);
-		ctx.fillText("Jump in the inverted fountain for good luck", canvas.width*.55, canvas.height*.75);
+		ctx.fillText("WASD or Arrow keys to move. Space to restart.", canvas.width*.1,canvas.height*.6);
+		ctx.fillText("Jump in the inverted fountain for good luck", canvas.width*.55, canvas.height*.55);
 		ctx.closePath();
 	}
 
 	function drawFloor() {
-		ctx.fillStyle = "#ffff99";
+		ctx.fillStyle = "#b8b2ad";
 		ctx.beginPath();
 		ctx.fillRect(0, ftnCanvasEdgeHeight, canvas.width, floorHeight); 
 		ctx.closePath();
@@ -73,6 +90,9 @@
 
 	function drawFtnMap() {
 
+
+		ctx.drawImage(uclaBackground, 0, 0);
+		ctx.drawImage(invertedFountain, ftnX, ftnY);
 		player1.draw();
 		drawFloor();
 		instructions();
@@ -81,8 +101,9 @@
 
 	function checkEndZone() {
 
-		if (player1.x+player1.width >= canvas.width) {
-			nextLevel();
+		if (player1.x < teleX + teleWidth  && player1.x + player1.width  > teleX &&
+			player1.y < teleY + teleHeight && player1.y + player1.height > teleY) {
+			nextLevel();	
 		}
 	}
 
